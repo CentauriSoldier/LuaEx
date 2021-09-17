854d33f801cb5f92fb0bf6e6a6ef377a8d3199d2
Centauri Soldier <3165890+CentauriSoldier@users.noreply.github.com>
2021-09-17 00:17:39 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,3 @@
 --package.path = package.path..";LuaEx\\?.lua;..\\?.lua;?.lua";
 package.path = package.path..";LuaEx\\?.lua";
 require("init");
-local p = function(v)
-	print(tostring(v).. " ("..type(v)..")")
-end
